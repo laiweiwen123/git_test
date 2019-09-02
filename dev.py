21a7532e5101b37234ccf48cf480464c91f204f5
laiweiwen123 <2579067284@qq.com>
2019-09-02 13:26:01 +0800

    分支开发已完成

---
+++ dev.py
@@ -0,0 +1,9 @@
+总用量 28
+drwxr-xr-x 3 python python 4096 9月   2 13:25 ./
+drwxr-xr-x 3 python python 4096 9月   1 19:19 ../
+-rw-r--r-- 1 python python    0 9月   2 13:25 dev.py
+drwxr-xr-x 8 python python 4096 9月   2 13:25 .git/
+-rw-r--r-- 1 python python 1203 9月   1 19:19 .gitignore
+-rw-r--r-- 1 python python  398 9月   2 12:53 lai.py
+-rw-r--r-- 1 python python 1069 9月   1 19:19 LICENSE
+-rw-r--r-- 1 python python   17 9月   2 12:33 manager_first.py
