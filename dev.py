a6eb5200e309ffca6dd1b1c7827cc9e0811ee2b8
经理 <jingli@qq.com>
2019-09-02 13:28:19 +0800

    分支以完成

--- dev.py
+++ dev.py
@@ -7,3 +7,12 @@ drwxr-xr-x 8 python python 4096 9月   2 13:25 .git/
 -rw-r--r-- 1 python python  398 9月   2 12:53 lai.py
 -rw-r--r-- 1 python python 1069 9月   1 19:19 LICENSE
 -rw-r--r-- 1 python python   17 9月   2 12:33 manager_first.py
+总用量 32
+drwxr-xr-x 3 python python 4096 9月   2 13:26 ./
+drwxr-xr-x 3 python python 4096 9月   1 19:14 ../
+-rw-r--r-- 1 python python  452 9月   2 13:26 dev.py
+drwxr-xr-x 8 python python 4096 9月   2 13:26 .git/
+-rw-r--r-- 1 python python 1203 9月   1 19:15 .gitignore
+-rw-r--r-- 1 python python  398 9月   2 13:12 lai.py
+-rw-r--r-- 1 python python 1069 9月   1 19:15 LICENSE
+-rw-r--r-- 1 python python   17 9月   2 12:28 manager_first.py
